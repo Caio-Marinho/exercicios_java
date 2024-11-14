664c6a76f911ef6293d6f62bbf56dc16804cb81b
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-14 18:19:38 -0300

    centralizando o pé dá arvore

--- src/exercicios/arvore_de_natal.java
+++ src/exercicios/arvore_de_natal.java
@@ -1,19 +1,25 @@
 package exercicios;
+import java.util.Scanner;
 
 public class arvore_de_natal {
 	public static void main(String[] args) {
-		int tamanho = 5;
-		for(int i = 1;i<=tamanho;i++) {
-			for (int j = 0; j<tamanho-i;j++) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Informe o tamanho da arvore: ");
+		int tamanho = entrada.nextInt();
+		int i;
+		int j;
+		int z=0;
+		for(i = 1;i<=tamanho;i++) {
+			for (j = 0; j<tamanho-i;j++) {
 				System.out.print(" ");
 			}
-			for (int z=0;z<2*i-1;z++) {
+			for (z=0;z<2*i-1;z++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		System.out.println(" ".repeat((tamanho/2)+1)+"*".repeat(2));
-		System.out.println(" ".repeat((tamanho/2)+1)+"*".repeat(2));
-		System.out.println(" ".repeat(tamanho/2)+"*".repeat(4));
+		System.out.println(" ".repeat((z/2)-1)+"*".repeat(2));
+		System.out.println(" ".repeat((z/2)-1)+"*".repeat(2));
+		System.out.println(" ".repeat((z/2)-2)+"*".repeat(4));
 	}
 }
