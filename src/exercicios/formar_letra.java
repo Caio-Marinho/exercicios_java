37af790836c8559feb9481584a1df22e6552481d
Caio-Marinho <kaigabriel12@gmail.com>
2024-11-19 11:59:10 -0300

    letra C

---
+++ src/exercicios/formar_letra.java
@@ -0,0 +1,10 @@
+package exercicios;
+
+public class formar_letra {
+	public static void main(String[] args) {
+		System.out.println("c".repeat(5));
+		System.out.println("c");
+		System.out.println("c");
+		System.out.println("c".repeat(5));
+	}
+}
